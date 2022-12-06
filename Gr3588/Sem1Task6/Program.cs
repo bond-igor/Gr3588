864156e7cd4dda8b65e949b92a82fd78f651e28c
Igor Bondarenko <bondarenko-i.v@ya.ru>
2022-12-06 16:25:13 +0600

    add Task6

---
+++ Gr3588/Sem1Task6/Program.cs
@@ -0,0 +1,21 @@
+﻿// Получаем значения от пользователя
+string? inputLineA = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if(inputLineA!=null)
+{
+    //Парсим введенное число
+    int numA = int.Parse(inputLineA);
+        
+    // Проверяем на четность
+    if(numA%2 == 0)
+    
+    //Выводим результат
+    {
+        Console.WriteLine("Четное");
+    }
+    else
+    {
+    Console.WriteLine("Нечетное");
+    }
+}
