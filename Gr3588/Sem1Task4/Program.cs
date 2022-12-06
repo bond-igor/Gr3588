01b6372e5ffea3f666eb7883b1970db76b1b2485
Igor Bondarenko <bondarenko-i.v@ya.ru>
2022-12-06 16:15:56 +0600

    add Task4

---
+++ Gr3588/Sem1Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Получаем значения от пользователя
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+string? inputLineC = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if(inputLineA!=null&&inputLineB!=null&&inputLineC!=null)
+{
+    //Парсим введенное число
+    int numA = int.Parse(inputLineA);
+    int numB = int.Parse(inputLineB);
+    int numC = int.Parse(inputLineC);
+    
+    //Сравниваем значения введенных чисел и выводим результат
+    Console.WriteLine("max=" +Math.Max(Math.Max(numA, numB), Math.Max(numB, numC)));
+    
+}
