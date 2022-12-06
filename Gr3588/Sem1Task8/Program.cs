d14837b04346fda2a787b00adb0d67b62e2d882b
Igor Bondarenko <bondarenko-i.v@ya.ru>
2022-12-06 16:54:37 +0600

    add Task8

---
+++ Gr3588/Sem1Task8/Program.cs
@@ -0,0 +1,29 @@
+﻿// Получаем значения от пользователя
+string? inputLineN = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if(inputLineN!=null)
+{
+    //Парсим введенное число
+    int numN = int.Parse(inputLineN);
+
+    //Добавляем точку отсчета
+    int numC = 2;
+
+    //Создаем накопитель результата
+    string res = string.Empty;
+
+    // Запускаем счетчик четных чисел
+    while (numC < numN + 1)
+    {
+        // Добавляем следующее число к результату
+        res = res + numC + ", ";
+
+        // Переходим к следующему четному числу
+        numC = numC + 2;
+    }        
+
+    //Выводим результат
+    Console.WriteLine(res);
+        
+}
