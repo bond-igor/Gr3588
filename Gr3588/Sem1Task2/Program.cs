794fd7fe102c20447664d1244a7fd9d17346ba8c
Igor Bondarenko <bondarenko-i.v@ya.ru>
2022-12-06 15:57:22 +0600

    add Task2

---
+++ Gr3588/Sem1Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Получаем значения от пользователя
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if(inputLineA!=null&&inputLineB!=null)
+{
+    //Парсим введенное число
+    int numA = int.Parse(inputLineA);
+    int numB = int.Parse(inputLineB);
+    
+    //Сравниваем значения введенных числе
+    if(numA>numB)
+    
+    //Выводим результат
+    {
+        Console.WriteLine($"{numA} больше чем {numB}");
+    }
+    else
+    {
+    Console.WriteLine($"{numB} больше чем {numA}");
+    }
+}
