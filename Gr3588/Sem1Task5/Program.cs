8dffe7178ce653f284158475e6679dabdaa1fa3d
Igor Bondarenko <bondarenko-i.v@ya.ru>
2022-12-06 15:15:04 +0600

    add Task7

---
+++ Gr3588/Sem1Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿string? inputLine = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if(inputLine!=null)
+{
+
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputLine);
+    int startNumber = inputNumber*(-1);
+
+    //Выходное значение
+    string outline = string.Empty;
+
+    while(startNumber<inputNumber)
+    {
+        outline = outline + startNumber + ", ";
+        startNumber++;
+    }
+    outline = outline + inputLine;
+
+    //Выводим данные в консоль
+    Console.WriteLine(outline);
+}
---
+++ Gr3588/Sem1Task7/Program.cs
@@ -0,0 +1,9 @@
+﻿string? inputLine = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if(inputLine!=null)
+{
+    int inputNumber = int.Parse(inputLine);
+    int lastDigit = inputNumber%10;
+    Console.WriteLine(lastDigit);
+}
