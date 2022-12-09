fdd9998c7c2216a49a817554bc6a9175099764b9
Igor Bondarenko <bondarenko-i.v@ya.ru>
2022-12-10 00:53:55 +0600

    add Task 10, 13, 15

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Ввод числа пользователем
+int number = int.Parse(Console.ReadLine()??"0");
+
+// Делаем проверку, что введенное число имеет три символа
+if(number > 99)
+{
+    // Находим количество цифр в числе
+    int quantity =(int)(Math.Log10(number) - 2);
+   
+    //Находим третью цифру
+    int result = number/(int)((Math.Pow(10, quantity)))%10;
+   
+    //выводим результат
+    Console.WriteLine("третье число " + result);
+}
+else
+{
+    // Если проверка не пройдена, запрашиваем новое число.
+    Console.WriteLine("В веденном числе менее трех символов");
+}
---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Ввод числа пользователем
+int numberDay = int.Parse(Console.ReadLine()??"0");
+
+//Проверяем чтобы число не выходило за диапазон кол-ва дней недели
+if(numberDay > 0 && numberDay < 8)
+{
+    // Проверяем является ли введенное значение выходным днем
+    if(numberDay > 5)
+
+    // Выводим результат
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день");
+    }
+}
+else
+{
+     Console.WriteLine("Введите число от 1 до 7");
+}
