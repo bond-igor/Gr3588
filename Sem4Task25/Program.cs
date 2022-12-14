dab09d1d67ab533c8935b6e08c289f90dca496b3
Igor Bondarenko <bondarenko-i.v@ya.ru>
2022-12-14 18:09:16 +0600

    add Task 24, 25, 26, 28, 30

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//___МЕТОДЫ_____
+
+//запрос данных у пользователя
+int ReadData(string msg)
+{
+    //выводим сообщение
+    Console.Write(msg);
+    //считываем число
+    return int.Parse(Console.ReadLine() ?? "0");//возвращаем значение
+  }
+
+// Метод вывода результата
+void PrintResult(string res)
+{
+   Console.WriteLine(res);
+}
+
+//Метод вычисления степени
+int CalcPow(int num, int pownum)
+{
+    int res = (int)Math.Pow(num, pownum);
+    return res;
+}
+
+//___ПРОГРАММА___
+
+int num = ReadData("Введите число:");
+int pownum = ReadData("Введите степень числа:");
+CalcPow(num, pownum);
+PrintResult(res);
