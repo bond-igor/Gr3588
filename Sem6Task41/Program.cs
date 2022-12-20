6547a1858662075387a0e8d32f51c7a54bbd1bba
Igor Bondarenko <bondarenko-i.v@ya.ru>
2022-12-20 19:41:03 +0600

    add Task 40-44

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// №41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//___МЕТОДЫ___
+//Метод, считывающий данные, введенные пользователем
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод, выводящий на печать данные пользователю
+void PrintData(string res, int value)
+{
+    Console.WriteLine(res + value);
+}
+
+//Метод считает колличество положительных чисел
+int CountPositiveNum(int num)
+{
+    int res = 0;
+    while(num > 0)
+    {
+        if (ReadData("Введите число") > 0) res++;
+        num = num - 1;
+    }
+    return res;
+}
+
+//___ПРОГРАММА___
+int num = ReadData("Введите общее колличество чисел ");
+int res = CountPositiveNum(num);
+PrintData("Колличество положительных чисел ", res);
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,38 @@
+﻿// №43 Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//___МЕТОДЫ___
+//Метод, считывающий данные, введенные пользователем
+double ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод, выводящий на печать данные пользователю
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+//Метод вычисляет координату точки пересечения прямых
+double CalcCoordX(double b1, double k1, double b2, double k2)
+{
+    double x = ((b2 - b1) / (k1 - k2));
+    return x;
+}
+
+double CalcCoordY(double x, double b2, double k2)
+{
+    double y = (k2*x + b2);
+    return y;
+}
+
+//___ПРОГРАММА___
+double b1 = ReadData("Введите b1 ");
+double k1 = ReadData("Введите k1 ");
+double b2 = ReadData("Введите b2 ");
+double k2 = ReadData("Введите k2 ");
+double coordX = CalcCoordX(b1,k1,b2,k2);
+double coordY = CalcCoordY(coordX, b2,k2);
+PrintData($"Координаты точки пересечения прямых: ({coordX}:{coordY})");
